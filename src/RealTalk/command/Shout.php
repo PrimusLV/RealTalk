962444af05b476eaeeb76939cc281ace0104adce
PrimusLV <k.drivnieks@inbox.lv>
2015-12-15 17:47:49 +0200

    Create Shout.php

---
+++ src/RealTalk/command/Shout.php
@@ -0,0 +1,9 @@
+<?php
+namespace RealTalk\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\PluginIdentifiableCommand;
+
+class Shout extends Command implements PluginIdentifiableCommand {
+
+}
